0418a4947a2b4571d121b7b95bae8e91dd9174f6
singhkunal2050 <singhkunal2050@gmail.com>
2021-12-28 18:11:46 +0530

    Added React Router

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,16 +1,6 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from "react-router-dom";
-
-import About from "./pages/About"
-import Tester from "./pages/Tester"
-import Home from "./pages/Home"
 
 
 function App() {
--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import Navbar from "./components/Navbar";
+
+import About from "./pages/About";
+import Tester from "./pages/Tester";
+import Home from "./pages/Home";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+render(
+  <Router>
+    <div>
+      <Navbar />
+
+      <hr />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/tester" element={<Tester/>} />
+      </Routes>
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
