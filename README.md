55fe8d221b63060964af17933c20d9555262d439
singhkunal2050 <singhkunal2050@gmail.com>
2021-12-28 21:09:22 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Acessibility
+
+
+- project on web accessibility
+react/node/auth/check accessibility
+herouko/oracle cloud 
+
+- mongo db  sqllite 
+- resume revamp 
--- public/src/pages/About.jsx
+++ public/src/pages/About.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="min-h-screen">
+    <div className="about-page min-h-screen">
       
       <h1 className='text-4xl font-extrabold '>About Page</h1>
+
+      
 
     </div>
   )
