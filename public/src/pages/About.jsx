a826e4ebea4443da1e8f44ce14b7eca7885bb21e
singhkunal2050 <singhkunal2050@gmail.com>
2022-01-04 20:22:18 +0530

    added content in about page

--- public/src/pages/About.jsx
+++ public/src/pages/About.jsx
@@ -4,9 +4,15 @@ function About() {
   return (
     <div className="about-page min-h-screen">
       
-      <h1 className='text-4xl font-extrabold '>About Page</h1>
+      <div className="container">
 
-      
+        <h1 className='text-4xl font-extrabold '>About Page</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint sed numquam ratione laboriosam saepe laborum placeat repellendus. Est, odit quia beatae voluptas eius ipsam vitae, ratione, natus fugiat fugit reiciendis repellendus numquam praesentium architecto illo provident. Cum reprehenderit doloribus cumque recusandae eligendi accusantium nam repellat, quibusdam natus veniam, repudiandae possimus earum similique laboriosam. Nostrum atque deserunt impedit odio ad, veniam voluptatem corrupti minima. Sint harum earum doloremque explicabo! Ipsa repellat ab minus incidunt et porro laudantium nostrum at? Natus saepe fuga nobis, expedita accusantium ipsa earum excepturi harum, esse eligendi doloremque. Optio placeat itaque blanditiis nihil obcaecati esse! Ipsa veniam sequi reiciendis tenetur qui aliquid! Iusto officiis illum obcaecati iste veniam delectus nostrum expedita odit animi vitae! Laborum repellat a nemo ullam nulla provident est porro, eum ipsa iste. Tempore ducimus fugiat voluptatibus, dolores quae, repellat magnam quis atque, sapiente ex aliquid. Optio dolores voluptas enim eaque repellat impedit reprehenderit dolorum, nemo, nesciunt consectetur, quibusdam fugit assumenda sint quasi nam voluptatum expedita. Aliquid iusto accusantium dicta laborum quo quia, voluptatum eaque cumque dolore quasi. Corporis in voluptatem deleniti beatae alias vitae optio omnis dicta, nesciunt exercitationem culpa assumenda itaque aliquam quia est maiores unde! Earum debitis veniam veritatis libero aliquam repellat aperiam sit dicta eos dolore odio maiores cum eum quam nobis soluta, adipisci architecto corrupti! Ipsa esse autem quidem, eius exercitationem minus totam ad eaque optio laudantium quo quos error laboriosam vel? Unde quam at officia praesentium dolor temporibus necessitatibus ipsa! Labore, enim dolorum. Quo, autem deserunt aspernatur facilis tempora ut asperiores soluta ullam odio culpa odit est corrupti repellat veritatis nisi sapiente nesciunt, dicta eligendi, libero dolor blanditiis eos numquam beatae reiciendis? Vitae error numquam, in accusantium reprehenderit expedita! Aperiam, vero laudantium commodi repellendus odio error? Odio, expedita fuga sed praesentium obcaecati sit quis blanditiis quo doloremque iusto?
+        </p>
+
+      </div>
 
     </div>
   )
