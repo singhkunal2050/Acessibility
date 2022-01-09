d2ba5559705f2143b15c9ba957c761e6b3823926
singhkunal2050 <singhkunal2050@gmail.com>
2022-01-09 20:43:02 +0530

    added animated blob with Tailwind

--- public/src/components/BlurBlob.jsx
+++ public/src/components/BlurBlob.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
+// Animated Blur Blobs
 function BlurBlob() {
   return (
-    <div className='blur-blog w-[40vw] h-[40vh] max-w-md max-h-80 bg-indigo-400 blur-[180px] absolute left-10 top-44 -z-10 '>
-
-    </div>
-  )
+    <aside>
+      <div className="blur-blog w-[40vw] h-[40vh] max-w-md max-h-80 bg-indigo-300 blur-[100px] absolute left-10 top-44 -z-10 mix-blend-multiply animate-wonder "></div>
+      <div className="blur-blog w-[40vw] h-[40vh] max-w-md max-h-80 bg-pink-300 blur-[100px] absolute left-20 top-14 -z-10 mix-blend-multiply animate-wonder delay-300 "></div>
+      <div className="blur-blog w-[40vw] h-[40vh] max-w-md max-h-80 bg-yellow-300 blur-[100px] absolute left-60 top-84 -z-10 mix-blend-multiply animate-wonder delay-1000 "></div>
+    </aside>
+  );
 }
 
-export default BlurBlob
+export default BlurBlob;
--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     './src/**/*.{vue,jsx,ts,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        'wonder': {
+          '0%': {
+            transform: 'translate(0,0) scale(1)'
+          },
+          '50%': {
+            transform: 'translate(140px,-80px) scale(1.2)'
+          },
+          '100%': {
+            transform: 'translate(0,0) scale(1)'
+          },
+      },
+      },
+
+      animation: {
+        'wonder': 'wonder 7s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
