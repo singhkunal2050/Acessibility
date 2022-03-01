d5776497622b801c398ce3aeee6684b79e01452e
singhkunal2050 <singhkunal2050@gmail.com>
2022-03-01 21:00:48 +0530

    added ddocs

--- public/src/pages/About.jsx
+++ public/src/pages/About.jsx
@@ -1,23 +1,71 @@
-import React from 'react'
+import React from "react";
+import ss from "../assets/img/ss.png";
 
 function About() {
   return (
     <div className="about-page min-h-screen">
-      
       <div className="container mx-auto">
+        <section className="prose-lg py-8">
+          <h1 className="text-4xl font-extrabold ">Web Accessibility Checker</h1>
+          <p>
+            Web accessibility means that people with disabilities can equally
+            perceive, understand, navigate, and interact with websites and
+            tools. It also means that they can contribute equally without
+            barriers. Accessibility is the concept of whether a product or
+            service can be used by everyone or not. Accessibility laws exist to
+            aid people with disabilities, but designers should try to
+            accommodate all potential users in many contexts of use anyway. To
+            do so has firm benefits—notably better designs for all. The
+            Accessibility Checker verifies your file against a set of rules that
+            identify possible issues for people who have disabilities or also
+            for normal people. Depending on how severe the issue is, the
+            Accessibility Checker classifies each issue as an error, warning, or
+            tip. The main objective of Accessibility Checker is a product or
+            service can be used by everyone. It makes websites more accessible
+            to all kind of user
+          </p>
+          <img src={ss} className="w-full border-2 border-indigo-400 p-4" alt="Acesssibility" />
 
-        <h1 className='text-4xl font-extrabold '>About Page</h1>
+          <h1 className="text-4xl font-extrabold">Client Dependency</h1>
+          <code>
+            <pre>
+              "react": "^17.0.2", <br />
+              "react-dom": "^17.0.2", <br />
+              "react-icons": "^4.3.1",<br />
+              "react-router-dom": "^6.2.1"<br />
+            </pre>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint sed numquam ratione laboriosam saepe laborum placeat repellendus. Est, odit quia beatae voluptas eius ipsam vitae, ratione, natus fugiat fugit reiciendis repellendus numquam praesentium architecto illo provident. Cum reprehenderit doloribus cumque recusandae eligendi accusantium nam repellat, quibusdam natus veniam, repudiandae possimus earum similique laboriosam. Nostrum atque deserunt impedit odio ad, veniam voluptatem corrupti minima. Sint harum earum doloremque explicabo! Ipsa repellat ab minus incidunt et porro laudantium nostrum at? Natus saepe fuga nobis, expedita accusantium ipsa earum excepturi harum, esse eligendi doloremque. Optio placeat itaque blanditiis nihil obcaecati esse! Ipsa veniam sequi reiciendis tenetur qui aliquid! Iusto officiis illum obcaecati iste veniam delectus nostrum expedita odit animi vitae! Laborum repellat a nemo ullam nulla provident est porro, eum ipsa iste. Tempore ducimus fugiat voluptatibus, dolores quae, repellat magnam quis atque, sapiente ex aliquid. Optio dolores voluptas enim eaque repellat impedit reprehenderit dolorum, nemo, nesciunt consectetur, quibusdam fugit assumenda sint quasi nam voluptatum expedita. Aliquid iusto accusantium dicta laborum quo quia, voluptatum eaque cumque dolore quasi. Corporis in voluptatem deleniti beatae alias vitae optio omnis dicta, nesciunt exercitationem culpa assumenda itaque aliquam quia est maiores unde! Earum debitis veniam veritatis libero aliquam repellat aperiam sit dicta eos dolore odio maiores cum eum quam nobis soluta, adipisci architecto corrupti! Ipsa esse autem quidem, eius exercitationem minus totam ad eaque optio laudantium quo quos error laboriosam vel? Unde quam at officia praesentium dolor temporibus necessitatibus ipsa! Labore, enim dolorum. Quo, autem deserunt aspernatur facilis tempora ut asperiores soluta ullam odio culpa odit est corrupti repellat veritatis nisi sapiente nesciunt, dicta eligendi, libero dolor blanditiis eos numquam beatae reiciendis? Vitae error numquam, in accusantium reprehenderit expedita! Aperiam, vero laudantium commodi repellendus odio error? Odio, expedita fuga sed praesentium obcaecati sit quis blanditiis quo doloremque iusto?
-        </p>
+          </code>
 
+          <h1 className="text-4xl font-extrabold">Server Dependency</h1>
+          <code>
+            <pre>
+            "cors": "^2.8.5", <br />
+            "express": "^4.17.2", <br />
+            "pa11y": "^6.1.1" <br />
+            </pre>
+
+          </code>
+
+          <h1 className="text-4xl font-extrabold">Software Requirements</h1>
+
+          <ul>
+          <li> Operating System: Windows, Linux.</li>
+          <li> Browser : Mozilla Firefox, Chrome, Safari.</li>
+          <li> Software Development Kit: VS Code</li>
+          <li> Language: React.js, Node.js</li>
+          </ul>
+          <h1 className="text-4xl font-extrabold">Hardware Requirements</h1>
+
+          <ul>
+          <li> Processor : Pentium.</li>
+          <li> RAM : 4GB.</li>
+          <li> Hard Disk : 10GB.</li>
+          </ul>
+        </section>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default About
-
-
+export default About;
