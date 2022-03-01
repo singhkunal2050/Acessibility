179f0310603cc389f70941ed0479bb5c37d20c19
singhkunal2050 <singhkunal2050@gmail.com>
2022-03-01 20:21:36 +0530

    added some sections

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import USP from '../components/USP'
 import Details from '../components/Details'
+import Cards from '../components/Cards'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
         <Hero/>
         <USP/>
         <Details/>
+        <Cards/>
 
     </section>
   )
